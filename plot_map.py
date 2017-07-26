b58dffe4a20f29c33339008f2522c6a48b6e0d9a
xuchangchen <imxuchang@gmail.com>
2017-07-26 00:56:45 -0700

    heatmap

--- plot_map.py
+++ plot_map.py
@@ -1,6 +1,8 @@
 import os
 import json
+import numpy as np
 import folium
+from folium.plugins import HeatMap
 from redis_client import redis_client
 
 def get_color(rate):
@@ -13,23 +15,20 @@ def get_color(rate):
     else:
         return "#ff3030"
 
-map_sf = folium.Map(location=[37.7676457, -122.4184106], zoom_start=13)
+lat_sf = 37.767645
+lng_sf = -122.4184106
 
+# regular map
+map_sf = folium.Map(location=[lat_sf, lng_sf], zoom_start=13)
 redis_db = redis_client()
 redis_db.connect()
+data = []
 for key in redis_db.conn.scan_iter():
     temp = redis_db.get(key)
     lat = temp["geo"]["lat"]
     lng = temp["geo"]["lng"]
     rate = temp["rate"]
     color = get_color(rate)
-    # folium.RegularPolygonMarker(
-    #     location=[lat, lng],
-    #     popup=str(rate) + " $/sqft",
-    #     fill_color=color,
-    #     number_of_sides=20,
-    #     radius=4
-    # ).add_to(map_sf)
     folium.CircleMarker(
         location=[lat, lng],
         popup=str(rate) + " $/sqft",
@@ -38,5 +37,10 @@ for key in redis_db.conn.scan_iter():
         color="black",
         radius=4
     ).add_to(map_sf)
-
+    data.append([lat, lng, rate])
 map_sf.save('map.html')
+
+# heatmap
+heatmap_sf = folium.Map([lat_sf, lng_sf], tiles='stamentoner', zoom_start=13)
+HeatMap(data).add_to(heatmap_sf)
+heatmap_sf.save('heatmap.html')
